a2b83301f77ab58486b7b036f3794bdef35b7f78
Aaron Smith <54414085+aaronsmith1203@users.noreply.github.com>
2022-07-20 17:41:01 +0100

    added app logic to `main()` method in `App` class
    - Added app logic so that the application can be executed with the
      features that have been implemented so far.
    - Utilises current `BasicCVLoader` concrete class. This needs to be
      refactored so that class 'codes to an interface' and the CV loader
      class can be replaced if needed.
    - So far there are no implementations of features necessary to export
      the loaded CV into another format, so the app logic outputs the loaded
      CV to the output stream to show that the load has occurred.
    - The path to the json file is expected as the first argument when
      running the application.
      `java -jar <path-to-jar> <path-to-json-file>`
    
    cvg-3

--- src/main/java/app/aaronsmith/cvgen/App.java
+++ src/main/java/app/aaronsmith/cvgen/App.java
@@ -1,13 +1,21 @@
 package app.aaronsmith.cvgen;
 
-/**
- * Hello world!
- *
- */
+import app.aaronsmith.cvgen.io.BasicCVLoader;
+import app.aaronsmith.cvgen.model.CV;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        try {
+            final String filePath = args[0]; // path to json file
+            final CV cv = BasicCVLoader.load(filePath);
+
+            // temporary sysout to show content loaded into CV class model
+            System.out.println(cv.toString());
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
